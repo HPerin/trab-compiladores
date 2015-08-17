efc17b9a7b3b34edea8af17bbf298c4a23d3c9dd
Lucas Perin <lhperin@gmail.com>
2015-08-17 20:27:51 -0300

    mudanca na estrutura de diretorios

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
-cd parts/p1
+cd etapa1
 make clean
 tar cvzf etapa1.tgz .
-cd ../../
-mv parts/p1/etapa1.tgz public_html/etapa1.tgz 
+cd ../
+mv etapa1/etapa1.tgz public_html/etapa1.tgz
 
 sftp lhperin@html.inf.ufrgs.br << END_TEXT
 put -r public_html
