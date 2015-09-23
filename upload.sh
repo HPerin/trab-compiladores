a8fc21dd8e23eddde97aba5fec9475dc8d08c432
Lucas Perin <lhperin@gmail.com>
2015-09-23 17:30:44 -0300

    enviado v1

--- upload.sh
+++ upload.sh
@@ -10,6 +10,16 @@ tar cvzf etapa2.tgz .
 cd ../
 mv etapa2/etapa2.tgz public_html/etapa2.tgz
 
+cd etapa3
+make clean
+tar cvzf etapa3.tgz .
+cd ../
+mv etapa3/etapa3.tgz public_html/etapa3.tgz
+
 sftp lhperin@html.inf.ufrgs.br << END_TEXT
 put -r public_html
+put public_html/etapa1.tgz public_html/etapa1.tgz
+put public_html/etapa2.tgz public_html/etapa2.tgz
+put public_html/etapa3.tgz public_html/etapa3.tgz
+put public_html/index.html public_html/index.html
 END_TEXT
