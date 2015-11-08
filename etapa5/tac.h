f08892aea0ed493cc282012ed1d54b0e0a395833
lfweigel <lucas.weigel@gmail.com>
2015-11-08 14:05:40 -0200

    typo

--- etapa5/tac.h
+++ etapa5/tac.h
@@ -7,11 +7,11 @@
 typedef struct tac_node
 {
 	int type;
-	hash_note_t *res;
-	hash_note_t *op1;
-	hash_note_t *op2;
+	hash_node_t *res;
+	hash_node_t *op1;
+	hash_node_t *op2;
 	struct tac_node *prev
-}tac_note_t
+}tac_node_t
 
-tac_note_t* tacCreate(int type, hash_node_t *res, hash_note_t *op1, hash_note_t* op2)
-tac_note_t* tacJoin(tac_node_t* TAC1, tac_node_t* TAC2);
+tac_node_t* tacCreate(int type, hash_node_t *res, hash_node_t *op1, hash_node_t* op2)
+tac_node_t* tacJoin(tac_node_t* TAC1, tac_node_t* TAC2);
