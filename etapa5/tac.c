30efc803a75174edaa7960a2b41ccaac12928054
lfweigel <lucas.weigel@gmail.com>
2015-11-09 09:41:36 -0200

    callv2

--- etapa5/tac.c
+++ etapa5/tac.c
@@ -504,9 +504,10 @@ tac_node_t* tacGenerate(ast_node_t *aux) {
 			break;
 
 		case FUNC_CALL: // 44
-			tac_aux1 = tacGenerate(ast_son_get(aux, 0));
-		
-			return tacJoin(tac_aux1, tacCreate(TAC_FUNCALL, hash_map_maketemp(hash), ast_son_get(aux, 0)->data, tac_aux1->res)); 
+			tac_aux1 = taGenerate(ast_son_get(aux,0));
+			hash_aux1 = hash_map_maketemp(hash);
+			
+			return tacJoin(tacJoin(tac_aux1, tacCreate(TAC_TEMP, hash_aux1, 0, 0)), tacCreate(TAC_CALL, hash_aux1, ast_son_get(aux, 0)->data, tac_aux1->data));
 			
 			//generate_code (output, ast_son_get(aux, 0)); // id
 			//fprintf (output, " (");
