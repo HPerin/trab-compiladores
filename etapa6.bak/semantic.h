fc4a17e35c22aea253e62f4ecb5e73b9e0cbe7c0
Lucas Perin <lucas@localhost.localdomain>
2015-12-04 20:23:45 -0200

    final commit ?

--- etapa6.bak/semantic.h
+++
@@ -1,5 +0,0 @@
-#include "ast.h"
-
-
-void checkDeclarations (ast_node_t* node, bool first_run);
-int hasSemanticErrors();
--- etapa6.bak/test.sh
+++
@@ -1,7 +0,0 @@
-make clean
-make
-./etapa6 test.txt out.s
-gcc -m32 -g -o out out.s
-gdb out << END_TEXT
-run
-END_TEXT
